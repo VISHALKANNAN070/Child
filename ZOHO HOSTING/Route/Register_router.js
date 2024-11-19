a38b3b06cc1eab32065eccd8b7fc9a596a000949
Vignesh M <vickymayilsamy@gmail.com>
2024-11-19 08:55:54 +0530

    Add files via upload

---
+++ ZOHO HOSTING/Route/Register_router.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const router = express.Router();
+const {create_table_register} = require("../Logics/Logics");
+router.post("/",create_table_register);
+
+module.exports=router;
---
+++ ZOHO HOSTING/Server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const mongoose = require('mongoose');
+const login_router = require("./Route/Login_router");
+const register_router = require("./Route/Register_router");
+const bodyParser = require('body-parser');
+
+// app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MongoDB_URL)
+    .then(()=>{
+        app.listen(process.env.X_ZOHO_CATALYST_LISTEN_PORT,()=>{
+            console.log("DataBase Successfully connected to port " + process.env.X_ZOHO_CATALYST_LISTEN_PORT);
+        });
+    })
+    .catch((e)=>{
+        console.log({error : e.message});
+    });
+
+    app.use("/login",login_router);
+    app.use("/register",register_router);
